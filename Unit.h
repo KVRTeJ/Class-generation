58cc08262beb5dd9258bd9441619e94f10bb596d
KVRTeJ <bb.sssss@inbox.ru>
2025-03-25 23:44:07 +0700

    Рефакторинг

--- Unit.h
+++ Unit.h
@@ -8,19 +8,20 @@ public:
     using Flags = unsigned int;
 public:
     virtual ~Unit() = default;
-    virtual void add( const std::shared_ptr< Unit >& , Flags ) {
+    virtual void add(const std::shared_ptr<Unit>& , Flags) {
         throw std::runtime_error( "Not supported" );
     }
-    virtual std::string compile( unsigned int level = 0 ) const =
-        0;
+
+    virtual std::string compile(unsigned int level = 0) const = 0;
+
 protected:
-    virtual std::string generateShift( unsigned int level ) const
-    {
+    virtual std::string generateShift(unsigned int level) const {
         static const auto DEFAULT_SHIFT = " ";
         std::string result;
-        for( unsigned int i = 0; i < level; ++i ) {
+
+        for( unsigned int i = 0; i < level; ++i )
             result += DEFAULT_SHIFT;
-        }
+
         return result;
     }
 };
