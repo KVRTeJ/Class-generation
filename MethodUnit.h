b857fb8568b0eec0e8606a17cadc660d7dd52e15
KVRTeJ <bb.sssss@inbox.ru>
2025-03-28 13:33:41 +0700

    Добавлены абстрактные классы, от которых, в дальнейшем, будут порождаться классы, которые генерируют код на определенном языке(C++, C#, Java)

---
+++ MethodUnit.h
@@ -0,0 +1,26 @@
+#ifndef METHODUNIT_H
+#define METHODUNIT_H
+
+#include "Unit.h"
+
+class MethodUnit : public Unit {
+public:
+    enum Modifier {
+        STATIC = 1,
+        CONST = 1 << 1,
+        VIRTUAL = 1 << 2
+    };
+
+public:
+    MethodUnit(const std::string &name, const std::string &returnType, Flags flags) :
+        m_name(name), m_returnType(returnType), m_flags(flags)
+    { }
+
+protected:
+    std::string m_name;
+    std::string m_returnType;
+    Flags m_flags;
+    std::vector< std::shared_ptr<Unit>> m_body;
+};
+
+#endif // METHODUNIT_H
