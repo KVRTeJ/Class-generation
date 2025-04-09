3eebdf3943e642625cfd8940faef11f046e7156f
KVRTeJ <bb.sssss@inbox.ru>
2025-04-09 23:52:12 +0700

    Рефакторинг метода compile. Исправлена ошибка при которой неправильно отоброжались отступы. Теперь модификаторы доступа выводятся с каждым методом

--- JavaClassUnit.h
+++ JavaClassUnit.h
@@ -25,16 +25,16 @@ public:
 
     std::string compile(unsigned int level = 0) const override {
         std::string result = generateShift(level) + (isAbstract ? "abstract " : "")
-                             + "class " + m_name + " {\n";
+        + "class " + m_name + " {\n";
 
         for(size_t i = 0; i < BASE_ACCESS_MODIFIERS.size(); ++i) {
             if(m_fields[i].empty()) {
                 continue;
             }
 
-            result += BASE_ACCESS_MODIFIERS[i] + ":\n";
             for(const auto &f : m_fields[i]) {
-                result += f->compile(level + 1);
+                result += BASE_ACCESS_MODIFIERS[i] + ' ';
+                result += f->compile(level);
             }
 
             result += "\n";
