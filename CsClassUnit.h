aae955b0826b244496df122946b04d2b38edf8d4
KVRTeJ <bb.sssss@inbox.ru>
2025-04-11 02:19:58 +0700

    Начало реализации генерации кода для классовна C#. Создан класс CsClassUnit и добавлен enum CsAccesModifier. В перечислении используется базовое перечисление ClassUnit::BaseAccessModifier, в идеале, я не хотел повторять код, но C++ не поддерживает полноценное расширение перечислений :(. Поэтому я использовал его, как основу для нового enum(пришлось вручную задавать константы)

--- CsClassUnit.h
+++ CsClassUnit.h
@@ -1,4 +1,34 @@
 #ifndef CSCLASSUNIT_H
 #define CSCLASSUNIT_H
 
+#include "ClassUnit.h"
+#include "CsMethodUnit.h"
+
+class CsClassUnit : public ClassUnit {
+public:
+    enum CsAccesModifier : Flags {
+        PUBLIC = BaseAccessModifier::PUBLIC,
+        PROTECTED = BaseAccessModifier::PROTECTED,
+        PRIVATE = BaseAccessModifier::PRIVATE,
+        PRIVATE_PROTECTED,
+        FILE,
+        INTERNAL,
+        PROTECTED_INTERNAL
+    };
+public:
+    explicit CsClassUnit(const std::string &name) : ClassUnit(name)
+    { }
+
+    void add(const std::shared_ptr<Unit> &unit, Flags flags = 0) override {
+
+    }
+
+    std::string compile(unsigned int level = 0) const override {
+        return {};
+    }
+
+private:
+    bool isAbstract = false;
+};
+
 #endif // CSCLASSUNIT_H
