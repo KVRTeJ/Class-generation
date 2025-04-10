b41b4043d62f6d217929833ded076d74864e945c
KVRTeJ <bb.sssss@inbox.ru>
2025-04-11 01:59:39 +0700

    Добавлено полное тестовое покрытие для CsMethodUnit

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+#include <assert.h>
+
 #include "CppClassUnit.h"
 #include "CppMethodUnit.h"
 #include "CppPrintOperatorUnit.h"
@@ -9,34 +11,53 @@
 #include "JavaClassUnit.h"
 
 #include "CsPrintOperatorUnit.h"
+#include "CsMethodUnit.h"
 
-std::string generateProgram() {
-    CppClassUnit myClass( "MyClass" );
-    myClass.add(
-        std::make_shared< CppMethodUnit >( "testFunc1", "void",  0),
-        CppClassUnit::BaseAccessModifier::PUBLIC
-        );
-    myClass.add(
-        std::make_shared< CppMethodUnit >( "testFunc2", "void", CppMethodUnit::STATIC ),
-        CppClassUnit::BaseAccessModifier::PRIVATE
-        );
-    myClass.add(
-        std::make_shared< CppMethodUnit >( "testFunc3", "void", CppMethodUnit::CONST |
-                                                                CppMethodUnit::VIRTUAL ),
-        CppClassUnit::BaseAccessModifier::PUBLIC
-        );
-    auto method = std::make_shared< CppMethodUnit >( "testFunc4", "void",
-                                                  0 );
-    method->add( std::make_shared< CppPrintOperatorUnit >( R"(Hello, world!\n)" ) );
-    myClass.add( method, CppClassUnit::BaseAccessModifier::PROTECTED);
+void testCsMethodUnit() {
+    // 1. Без флагов
+    CsMethodUnit test1("test1", "void");
+    assert(test1.compile() == "void test1() {\n}\n");
 
-    return myClass.compile();
+    // 2. ABSTRACT
+    CsMethodUnit test2("test2", "void", CsMethodUnit::ABSTRACT);
+    assert(test2.compile() == "abstract void test2() {\n}\n");
+
+    // 3. SEALED
+    CsMethodUnit test3("test3", "void", CsMethodUnit::SEALED);
+    assert(test3.compile() == "sealed void test3() {\n}\n");
+
+    // 4. STATIC
+    CsMethodUnit test4("test4", "void", CsMethodUnit::STATIC);
+    assert(test4.compile() == "static void test4() {\n}\n");
+
+    // 5. VIRTUAL
+    CsMethodUnit test5("test5", "void", CsMethodUnit::VIRTUAL);
+    assert(test5.compile() == "virtual void test5() {\n}\n");
+
+    // 6. CONST
+    CsMethodUnit test6("test6", "void", CsMethodUnit::CONST);
+    assert(test6.compile() == "const void test6() {\n}\n");
+
+    // 7. READONLY
+    CsMethodUnit test7("test7", "void", CsMethodUnit::READONLY);
+    assert(test7.compile() == "readonly void test7() {\n}\n");
+
+    // 8.
+    CsMethodUnit test8("test8", "void", CsMethodUnit::ABSTRACT | CsMethodUnit::CONST);
+    assert(test8.compile() == "abstract const void test8() {\n}\n");
+
+    CsMethodUnit test9("test9", "void", CsMethodUnit::STATIC | CsMethodUnit::VIRTUAL);
+    assert(test9.compile() != "virtual static void test9() {\n}\n");
+
+    CsMethodUnit test10("test10", "void", CsMethodUnit::SEALED | CsMethodUnit::READONLY);
+    assert(test10.compile() == "sealed readonly void test10() {\n}\n");
+
+    std::cout << "All tests passed!" << std::endl;
 }
 
 int main() {
 
-    CsPrintOperatorUnit boo("C#: Hello, world!");
-    std::cout << boo.compile() << std::endl;
+    testCsMethodUnit();
 
     return 0;
 }
