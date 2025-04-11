7c55d447bec84ec50b1b3a08efef09283d13b9c5
KVRTeJ <bb.sssss@inbox.ru>
2025-04-11 09:55:27 +0700

    Добавлена реализация абстрактной фабрики: Абстрактрые продукты - ClassUnit, MethodUnit, PrintOperatorUnit, Конкрентные продукты - их реализация на различных языках. Абстрактная фабрика - iFactory, Конкретные фабрики - CppFactory, JavaFactory, CsFactory

--- Factory.h
+++ Factory.h
@@ -1,4 +1,65 @@
 #ifndef FACTORY_H
 #define FACTORY_H
 
+#include <memory>
+
+#include "CppPrintOperatorUnit.h"
+#include "CppMethodUnit.h"
+#include "CppClassUnit.h"
+
+#include "JavaPrintMethodUnit.h"
+#include "JavaMethodUnit.h"
+#include "JavaClassUnit.h"
+
+#include "CsPrintOperatorUnit.h"
+#include "CsMethodUnit.h"
+#include "CsClassUnit.h"
+
+class iFactory {
+public:
+    virtual std::shared_ptr<ClassUnit> makeClass(const std::string& name) = 0;
+    virtual std::shared_ptr<MethodUnit> makeMethod(const std::string& name, const std::string& returnType, Unit::Flags flags) = 0;
+    virtual std::shared_ptr<PrintOperatorUnit> makePrintOperator(const std::string& text) = 0;
+};
+
+class CppFactory : public iFactory {
+public:
+    std::shared_ptr<ClassUnit> makeClass(const std::string& name) override {
+        return std::make_shared<CppClassUnit>(name);
+    }
+    std::shared_ptr<MethodUnit> makeMethod(const std::string& name, const std::string& returnType, Unit::Flags flags) override {
+        return std::make_shared<CppMethodUnit>(name, returnType, flags);
+    }
+    std::shared_ptr<PrintOperatorUnit> makePrintOperator(const std::string& text) override {
+        return std::make_shared<CppPrintOperatorUnit>(text);
+    }
+};
+
+class JavaFactory : public iFactory {
+public:
+    std::shared_ptr<ClassUnit> makeClass(const std::string& name) override {
+        return std::make_shared<JavaClassUnit>(name);
+    }
+    std::shared_ptr<MethodUnit> makeMethod(const std::string& name, const std::string& returnType, Unit::Flags flags) override {
+        return std::make_shared<JavaMethodUnit>(name, returnType, flags);
+    }
+    std::shared_ptr<PrintOperatorUnit> makePrintOperator(const std::string& text) override {
+        return std::make_shared<JavaPrintOperatorUnit>(text);
+    }
+};
+
+class CsFactory : public iFactory {
+public:
+    std::shared_ptr<ClassUnit> makeClass(const std::string& name) override {
+        return std::make_shared<CsClassUnit>(name);
+    }
+    std::shared_ptr<MethodUnit> makeMethod(const std::string& name, const std::string& returnType, Unit::Flags flags) override {
+        return std::make_shared<CsMethodUnit>(name, returnType, flags);
+    }
+    std::shared_ptr<PrintOperatorUnit> makePrintOperator(const std::string& text) override {
+        return std::make_shared<CsPrintOperatorUnit>(text);
+    }
+};
+
+
 #endif // FACTORY_H
--- main.cpp
+++ main.cpp
@@ -1,32 +1,27 @@
 #include <iostream>
 
-#include <assert.h>
+#include "Factory.h"
 
-#include "CppClassUnit.h"
-#include "CppMethodUnit.h"
-#include "CppPrintOperatorUnit.h"
-
-#include "JavaPrintMethodUnit.h"
-#include "JavaMethodUnit.h"
-#include "JavaClassUnit.h"
-
-#include "CsPrintOperatorUnit.h"
-#include "CsMethodUnit.h"
-#include "CsClassUnit.h"
+std::string generateProgram(iFactory *factory) {
+    std::shared_ptr<ClassUnit> myClass = factory->makeClass("MyClass");
+    myClass->add(
+        factory->makeMethod( "testFunc1", "void", 0 ),
+        ClassUnit::PUBLIC
+        );
+    myClass->add(
+        factory->makeMethod("testFunc2", "void", 1),
+        ClassUnit::PRIVATE
+        );
+    myClass->add(
+        factory->makeMethod("testFunc3", "void", 1 | (1 << 2)),
+        ClassUnit::PUBLIC
+        );
+    auto method = factory->makeMethod("testFunc4", "void", 1);
+    // method->add(factory->makePrintOperator("Hello, world!\n"));
+    myClass->add(method, (1 << 3));
+    return myClass->compile();
+}
 
 int main() {
 
-    JavaMethodUnit boo("JAVA_IS_HERE", "void", (JavaMethodUnit::FINAL | JavaMethodUnit::STATIC));
-    boo.add(std::make_shared<JavaPrintOperatorUnit>(R"(Hello, world!\n)"));
-    boo.add(std::make_shared<JavaPrintOperatorUnit>(R"(Bye, world!\n)"));
-
-    JavaMethodUnit boo1("JAVA_IS_HERE2", "string", (JavaMethodUnit::ABSTRACT | JavaMethodUnit::STATIC));
-    JavaMethodUnit boo2("JAVA_IS_HERE3", "int", (JavaMethodUnit::STATIC));
-
-    JavaClassUnit foo("JavaClass");
-    foo.add(std::make_shared<JavaMethodUnit>(boo), 0);
-    foo.add(std::make_shared<JavaMethodUnit>(boo1), 2);
-    foo.add(std::make_shared<JavaMethodUnit>(boo2), 0);
-    std::cout << foo.compile();
-    return 0;
 }
